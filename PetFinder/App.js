1f56132107bdb6da721ee755824b9a5676a83fcd
Grace Chen <75271809+gracexichen@users.noreply.github.com>
2024-04-28 02:44:04 -0700

    nav bar with icons

--- PetFinder/App.js
+++ PetFinder/App.js
@@ -1,18 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
 import Tabs from './BottomNavigation/navigation';
 
 export default function App() {
   return (
-    <Tabs></Tabs>
+    <Tabs/>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
--- PetFinder/BottomNavigation/navigation.js
+++ PetFinder/BottomNavigation/navigation.js
@@ -1,24 +1,75 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import {NavigationContainer} from "@react-navigation/native";
+import { Image, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+import { StyleSheet } from "react-native";
 
-import Listing from '../Listing/listing';
-import Map from '../Map/map';
-import Profile from '../Profile/profile';
-import Sighting from '../Sighting/sighting';
+import Listing from "../Listing/listing";
+import Map from "../Map/map";
+import Profile from "../Profile/profile";
+import Sighting from "../Sighting/sighting";
 
 const Tab = createBottomTabNavigator();
 
 const Tabs = () => {
-    return(
-        <NavigationContainer>
-            <Tab.Navigator>
-                <Tab.Screen name = "Listing" component = {Listing}/>
-                <Tab.Screen name = "Map" component= {Map}/>
-                <Tab.Screen name = "Profile" component= {Profile}/>
-                <Tab.Screen name = "Sighting" component= {Sighting}/>
-            </Tab.Navigator>
-        </NavigationContainer>
-    );
-}
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen
+          name="Listing"
+          component={Listing}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({focused}) => (
+              <View style={{}}>
+                <Image source={focused? require("../Listing/listing_highlighted.png") : require("../Listing/listing.png")} />
+              </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Map"
+          component={Map}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({focused}) => (
+                <View style={{}}>
+                  <Image source={focused? require("../Map/map_highlighted.png") : require("../Map/map.png")} />
+                </View>
+              ),
+          }}
+        />
+        <Tab.Screen
+          name="Sighting"
+          component={Sighting}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: ({focused}) => (
+                <View style={{}}>
+                  <Image source={focused? require("../Sighting/sighting_highlighted.png") : require("../Sighting/sighting.png")} />
+                </View>
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarShowLabel: false,
+            tabBarIcon: () => (
+              <View style={{}}>
+                <Image source={require("../Profile/profile.png")} />
+              </View>
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+};
 
+const styles = StyleSheet.create({
+    container:{
+        height: '500',
+    }
+})
 export default Tabs;
